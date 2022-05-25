b8c6f841dfde7113faa81dbc1ac59a6a93e00439
vicvitoria666 <https.vitoriaoliveira@gmail.com>
2022-05-25 17:35:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    number: 0
+  }
+
+  add = () => {
+    var secInt = setInterval(() => {
+
+      this.setState({
+        number: this.state.number < 1000 ? this.state.number + 1 : this.state.num
+      });
+    }, 1000)
+    this.stopInt = () => {clearInterval(secInt)}
+  };
+
+
+
+  reset = () => {
+    this.setState({
+      number: 0 
+    })
+    this.stopInt()
+  }
+
+
+  render() {
+    
+    
+    return (
+      <div className="container">
+      <div className="number">
+        <h1> {this.state.number} </h1>
+      </div>
+      <div className="buttonBox">
+        <button className="button" onClick={this.add}>Start</button>
+        <button className="button" onClick={this.reset}>Reset</button>
+        <button className="button" onClick={this.stopInt}>Stop</button>
+      </div>
+      <p className="naoNasciDesigner">a estilização vai ser quando eu tiver criatividade</p>
+      </div>
+    )
+  }
 }
-
-export default App;
